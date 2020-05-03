9b259b0e6a41629efc10410904f07250c4a33b2c
Edy <edyxicon@gmail.com>
2020-05-02 20:03:55 -0600

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 require('./config/config');
 
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
+
+
+app.use(require('./routes/usuario'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,34 +16,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.get('/usuario', (req, res) => {
-  res.json('get Usuario');
-});
 
-app.post('/usuario', (req, res) => {
-  let body = req.body;
 
-  if ( body.nombre === undefined ) {
-    res.status(400).json({
-      ok: false,
-      mensaje: "El nombre es necesario."
-    });
-  } else {
+// Conectar Base de Datos
 
-    res.json({body});
-  }
+mongoose.connect(process.env.URLDB, 
+                { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+                (err, res) => {
+  if ( err ) throw err;
 
-});
-
-app.put('/usuario/:id', (req, res) => {
-  let id = req.params.id;
-  res.json({
-    id
-  });
-});
-
-app.delete('/usuario', (req, res) => {
-  res.json('delete Usuario');
+  console.log("Base de datos ONLINE");
 });
 
 app.listen(process.env.PORT, ()=> {
